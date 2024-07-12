9f90f5ff5dddb5df0ac740e3983be1dc127bc9e3
Alex Gordon <alexg1991@outlook.com>
2024-07-12 16:26:25 +0000

    Add buttons with functions to change colour using 'this'

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $('p').on("click",function() {
-    $('p').slideToggle('slow');
-})
+    $(this).slideToggle('slow');
+});
+/* using 'this' ensures that only the element which the user clicked on reacts
+and not all the same elements if only one of them is clicked */
+
+$('button').on("mouseenter", function() {
+    $(this).removeClass('makeRed').addClass('makeBlue');
+});
+$('button').on("mouseleave", function() {
+    $(this).removeClass('makeBlue').addClass('makeRed');
+});
